77b69164f5dfd5fc0980e30cd364e892eb5b39e8
Yeran Ruveesha Srinayaka <ruveeshayeran@gmail.com>
2025-11-29 23:08:19 +0530

    fix import path for useAuth in NavbarVendor component

--- ayubowan-connect- next js/components/header_footer/header_footer/NavbarVendor.tsx
+++ ayubowan-connect- next js/components/header_footer/header_footer/NavbarVendor.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../../../context/AuthContext';
 
 interface NavbarVendorProps {
   textColorClass?: string;
