5b0a60267914fdfaafa709f0126c9ce70e5c9180
Yeran Ruveesha Srinayaka <ruveeshayeran@gmail.com>
2025-11-29 22:26:58 +0530

    fix import path for AuthContext in GlobalHeader component

--- ayubowan-connect- next js/components/header_footer/header_footer/GlobalHeader.tsx
+++ ayubowan-connect- next js/components/header_footer/header_footer/GlobalHeader.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../../../context/AuthContext';
 import NavbarGuest from './NavbarGuest';
 import NavbarTraveller from './NavbarTraveller';
 import NavbarVendor from './NavbarVendor';
