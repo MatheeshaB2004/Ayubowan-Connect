8d56eee0a77b8d16f7e0ff3a230eeb3902d5cb51
Dulesini Jayathilaka <96082917+dulesinij@users.noreply.github.com>
2026-01-20 18:17:41 +0530

    feature(events): setup events module

---
+++ backend/src/modules/events/events.module.ts
@@ -0,0 +1,9 @@
+import { Modules } from '@nestjs/common';
+import { EventsController } from './events.controller';
+import { {EventsService } from './events.service';
+
+@Module({
+    controllers: [EventsController],
+    providers: [EventsService],
+})
+export class EventsModule {}
