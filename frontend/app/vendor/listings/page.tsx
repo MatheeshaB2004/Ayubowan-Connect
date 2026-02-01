0460500111af78e5ea0ee9fa9a104f4d91e33d20
YenulkaDesilva <yenulka.20240099@iit.ac.lk>
2026-02-01 09:18:38 +0530

    Add vendor create listings page skeleton

---
+++ frontend/app/vendor/listings/page.tsx
@@ -0,0 +1,8 @@
+export default function ManageListingsPage() {
+  return (
+    <div>
+      <h1>Create & Manage Listings</h1>
+      <p>This page allows vendors to create and manage their listings.</p>
+    </div>
+  );
+}
