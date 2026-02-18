f7830ee1066e200cfd97b25f22efe74515fc75d9
Dulesini Jayathilaka <96082917+dulesinij@users.noreply.github.com>
2026-02-18 21:29:21 +0530

    update create event dto

--- backend/src/modules/events/dto/create-event.dto.ts
+++ backend/src/modules/events/dto/create-event.dto.ts
@@ -1,4 +1,34 @@
+import { IsString, IsDateString, IsOptional, IsEnum } from 'class-validator';
+import { EventStatus } from '@prisma/client';
+
 export class CreateEventDto {
+  @IsString()
   title: string;
-  description: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsDateString()
+  startDate: string;
+
+  @IsOptional()
+  @IsDateString()
+  endDate?: Date;
+
+  @IsString()
+  location: string;
+
+  @IsString()
+  city: string;
+
+  @IsString()
+  district: string;
+
+  @IsString()
+  province: string;
+
+  @IsOptional()
+  @IsEnum(EventStatus)
+  status?: EventStatus;
 }
