c4f221984fc804c9f7e75cf35a454eebf7ca6fb3
niveka-wijeratne <niveka.20240734@iit.ac.lk>
2026-03-07 15:26:37 +0530

    chore(backend): update module configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { MarketplaceModule } from './modules/marketplace/marketplace.module';
-// import { PaymentsModule } from './modules/payments/payments.module';
+import { CartModule } from './modules/cart/cart.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MarketplaceModule,
-    // PaymentsModule, // ⛔ TEMP DISABLED
+    CartModule,
   ],
 })
 export class AppModule {}
