43e18e23f7a8621a8ea59856b40a82f3e5717097
YenulkaDesilva <yenulka.20240099@iit.ac.lk>
2026-03-21 15:28:18 +0530

    Updated package lock json

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log("DATABASE_URL:", process.env.DATABASE_URL);
   const app = await NestFactory.create(AppModule);
 
   // Enable global validation
